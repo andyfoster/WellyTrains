ccdf918cd6a4987bfeb070247eb1aa8ff664f1f6
Andy Foster <andrewfosteris@gmail.com>
2023-09-01 16:08:31 +1200

    Refactor Station class and add coordinates
    - Refactored the Station class to improve code readability and maintainability.
    - Added instance variables for the coordinates of the station (topLeft, topRight, bottomLeft, bottomRight).
    - Created a setter method `setCoords` to set the coordinates of the station.
    - Updated relevant methods to use the new instance variables.
    
    This commit improves the Station class by adding support for storing and retrieving station coordinates.

--- src/Station.java
+++ src/Station.java
@@ -4,57 +4,68 @@
 
 // Code for SWEN502, Assignment W2
 
-import java.util.*;
-import java.io.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Station
- * Information about an individual station:
- * - The name
- * - The fare zone it is in (1 - 14)
- * - The distance from the hub station (Wellington)
- * - The set of TrainLines that go through that station.
- * The constructor just takes the name, zone and distance;
- * TrainLines must then be added to the station, one by one.
+ * Station Information about an individual station: - The name - The fare zone
+ * it is in (1 - 14) - The distance from the hub station (Wellington) - The set
+ * of TrainLines that go through that station. The constructor just takes the
+ * name, zone and distance; TrainLines must then be added to the station, one by
+ * one.
  */
 
-public class Station{
+public class Station {
 
-    private String name;  
-    private int zone;          // fare zone
-    private double distance;   // distance from Wellington
-    private Set<TrainLine> trainLines = new HashSet<TrainLine>();  
+	private String name;
+	private int zone; // fare zone
+	private double distance; // distance from Wellington
+	private Set<TrainLine> trainLines = new HashSet<>();
 
-    public Station(String name, int zone, double dist){
-        this.name = name;
-        this.zone = zone;
-        this.distance = dist;
-    }
+	private double topLeft;
+	private double topRight;
+	private double bottomLeft;
+	private double bottomRight;
 
-    public String getName(){
-        return this.name;
-    }
+	public Station(String name, int zone, double dist) {
+		this.name = name;
+		this.zone = zone;
+		this.distance = dist;
+	}
 
-    public int getZone(){
-        return this.zone;
-    }
+	public void setCoords(double topLeft, double topRight, double bottomLeft, double bottomRight) {
+		this.topLeft = topLeft;
+		this.topRight = topRight;
+		this.bottomLeft = bottomLeft;
+		this.bottomRight = bottomRight;
+	}
 
-    /**
-     * Add a TrainLine to the station
-     */
-    public void addTrainLine(TrainLine line){
-        trainLines.add(line);
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public Set<TrainLine> getTrainLines(){
-        return Collections.unmodifiableSet(trainLines); //Return an unmodifiable version of the set of train lines.
-    }
+	public int getZone() {
+		return this.zone;
+	}
 
-    /**
-     * toString is the station name plus zone, plus number of train lines
-     */
-    public String toString(){
-        return name+" (zone "+zone+", "+trainLines.size()+" lines)";
-    }
+	/**
+	 * Add a TrainLine to the station
+	 */
+	public void addTrainLine(TrainLine line) {
+		trainLines.add(line);
+	}
+
+	public Set<TrainLine> getTrainLines() {
+		return Collections.unmodifiableSet(trainLines); // Return an unmodifiable version of the set of train lines.
+	}
+
+	/**
+	 * toString is the station name plus zone, plus number of train lines
+	 */
+	@Override
+	public String toString() {
+		return name + " (zone " + zone + ", " + trainLines.size() + " lines)";
+	}
 
 }
