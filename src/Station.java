0bf17fb49d33a3708d8d9736ac7de2622eaaba4c
Andy Foster <andrewfosteris@gmail.com>
2023-09-03 00:47:23 +1200

    Refactor Station class for improved readability and functionality
    - Renamed variables topLeft, topRight, bottomLeft, and bottomRight to leftEdge, rightEdge, topEdge, and bottomEdge respectively
    - Added methods getLeftEdge(), getRightEdge(), getTopEdge(), and getBottomEdge() to retrieve the coordinates of the station edges
    - Added method isExchange() to determine if a station is an exchange based on the number of train lines it has
    - Added methods calculateWidth() and calculateHeight() to calculate the width and height of the station respectively

--- src/Station.java
+++ src/Station.java
@@ -18,54 +18,82 @@ import java.util.Set;
 
 public class Station {
 
-	private String name;
-	private int zone; // fare zone
-	private double distance; // distance from Wellington
-	private Set<TrainLine> trainLines = new HashSet<>();
+  private String name;
+  private int zone; // fare zone
+  private double distance; // distance from Wellington
+  private Set<TrainLine> trainLines = new HashSet<>();
 
-	private double topLeft;
-	private double topRight;
-	private double bottomLeft;
-	private double bottomRight;
+  private double leftEdge;
+  private double rightEdge;
+  private double topEdge;
+  private double bottomEdge;
 
-	public Station(String name, int zone, double dist) {
-		this.name = name;
-		this.zone = zone;
-		this.distance = dist;
-	}
+  public Station(String name, int zone, double dist) {
+    this.name = name;
+    this.zone = zone;
+    this.distance = dist;
+  }
 
-	public void setCoords(double topLeft, double topRight, double bottomLeft, double bottomRight) {
-		this.topLeft = topLeft;
-		this.topRight = topRight;
-		this.bottomLeft = bottomLeft;
-		this.bottomRight = bottomRight;
-	}
+  public void setCoords(double leftEdge, double topEdge, double rightEdge, double bottomEdge) {
+    this.leftEdge = leftEdge;
+    this.rightEdge = rightEdge;
+    this.topEdge = topEdge;
+    this.bottomEdge = bottomEdge;
+  }
 
-	public String getName() {
-		return this.name;
-	}
+  public double getLeftEdge() {
+    return this.leftEdge;
+  }
 
-	public int getZone() {
-		return this.zone;
-	}
+  public double getRightEdge() {
+    return this.rightEdge;
+  }
 
-	/**
-	 * Add a TrainLine to the station
-	 */
-	public void addTrainLine(TrainLine line) {
-		trainLines.add(line);
-	}
+  public double getTopEdge() {
+    return this.topEdge;
+  }
 
-	public Set<TrainLine> getTrainLines() {
-		return Collections.unmodifiableSet(trainLines); // Return an unmodifiable version of the set of train lines.
-	}
+  public double getBottomEdge() {
+    return this.bottomEdge;
+  }
 
-	/**
-	 * toString is the station name plus zone, plus number of train lines
-	 */
-	@Override
-	public String toString() {
-		return name + " (zone " + zone + ", " + trainLines.size() + " lines)";
-	}
+  public boolean isExchange() {
+    return this.trainLines.size() > 2;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getZone() {
+    return this.zone;
+  }
+
+  public double calculateWidth() {
+    return this.rightEdge - this.leftEdge;
+  }
+
+  public double calculateHeight() {
+    return this.bottomEdge - this.topEdge;
+  }
+
+  /**
+   * Add a TrainLine to the station
+   */
+  public void addTrainLine(TrainLine line) {
+    trainLines.add(line);
+  }
+
+  public Set<TrainLine> getTrainLines() {
+    return Collections.unmodifiableSet(trainLines); // Return an unmodifiable version of the set of train lines.
+  }
+
+  /**
+   * toString is the station name plus zone, plus number of train lines
+   */
+  @Override
+  public String toString() {
+    return name + " (zone " + zone + ", " + trainLines.size() + " lines)";
+  }
 
 }
