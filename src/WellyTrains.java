01e09757a53ff63a7c7175557cddf8e57155f7ca
Andy Foster <andrewfosteris@gmail.com>
2023-09-03 20:08:35 +1200

    Add findRouteBtn and updateButtons method
    - Added a new button called findRouteBtn to the UI.
    - Implemented the logic for finding a route between two stations on the same line.
    - Created a new method called updateButtons to update the text of currentStationBtn.

--- src/WellyTrains.java
+++ src/WellyTrains.java
@@ -1,11 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -22,6 +17,7 @@ public class WellyTrains {
   private JButton currentStationBtn;
   private JButton destStationBtn;
   private JButton listAllLinesBtn;
+  private JButton findRouteBtn;
 
   private double pressedX = 0;
   private double pressedY = 0;
@@ -33,13 +29,11 @@ public class WellyTrains {
   public void setupUserInterface() {
     UI.initialise();
     UI.setWindowSize(1000, 750);
-    UI.addButton("Clear", () -> {
-      this.resetScreen();
-    });
+    UI.addButton("Clear", this::resetScreen);
 
     UI.addButton("All Stations in Region", this::showAllStations);
     UI.addButton("All Train Lines in Region", this::showAllTrainLines);
-    UI.addButton("Show All Info For All Train Lines", this::showAllInfoAllLines);
+    // UI.addButton("Show All Info For All Train Lines", this::showAllInfoAllLines);
 
     UI.addButton("", null);
 
@@ -50,12 +44,13 @@ public class WellyTrains {
     listAllLinesBtn = UI.addButton("List All Lines through " + this.currentStationName,
         this::printSimpleLinesForStation);
 
-    // UI.addButton("Print lines and stations on line through current station",
-    // this::this::listAllLinesThroughStation);
+    findRouteBtn = UI.addButton("Find a route from " + currentStationName + " to " + destinationStationName,
+        this::findARoute);
 
-    UI.addButton("Find a route from Current to Destination", this::findARoute);
+    UI.addButton("Next services from current station", this::findNextServiceOnEachLine);
 
     this.resetScreen();
+    this.printInstructions();
     UI.setMouseListener(this::mouseListener);
   }
 
@@ -67,14 +62,13 @@ public class WellyTrains {
   public void setCurrentTime() {
 
     // Ask the user for time input
-    final String timeInput = JOptionPane.showInputDialog(null, "Enter the time in format HHMM", "1530",
+    final String timeInput = JOptionPane.showInputDialog(null, "Enter the time in format HHMM", "Enter Time",
         JOptionPane.PLAIN_MESSAGE);
     this.currentTime = timeInput;
 
     // Validate the time input
     // It will be in string HHMM form but it should still be a valid time
     // E.g. 1530 is valid but 2560 is not
-
     // Here are a few checks but I am sure there are more
 
     if (timeInput.length() != 4) { // stops 124
@@ -105,107 +99,125 @@ public class WellyTrains {
     // UI.printMessage("You entered time: " + time);
   }
 
-  public void timeInputListener(final String time) {
-    // UI.println(time);
-    this.currentTime = time;
+  /*
+   * Find the next train service for each line at the current station immediately
+   * after a
+   * user specified time
+   */
+  public void findNextServiceOnEachLine() {
+
+    UI.println("-- Showing next time on each line through " + currentStationName + " station --");
+    final Station s = stations.get(currentStationName);
+    for (final TrainLine tl : s.getTrainLines()) {
+      int currentIndex = tl.getStations().indexOf(s);
+      int nextServiceTime = -1;
+      for (final TrainService service : tl.getTrainServices()) {
+        int currentTimeInt = Integer.parseInt(currentTime);
+        int currentServiceTime = service.getTimes().get(currentIndex);
+        if (currentServiceTime > currentTimeInt) {
+          nextServiceTime = currentServiceTime;
+          break;
+        }
+      }
+      UI.println(tl.getName() + ": " + addColonToTime(nextServiceTime));
+    }
   }
 
   /*
-   * Tries to find a route between two stations
+   * Adds a colon to a time in the format HHMM to HH:MM
+   */
+  public String addColonToTime(int time) {
+    String timeStr = Integer.toString(time);
+    return timeStr.substring(0, 2) + ":" + timeStr.substring(2, 4);
+  }
+
+  /**
+   * Finds a route between two stations.
    */
   public void findARoute() {
     boolean onSameLine = false;
+    Station currentStation = stations.get(currentStationName);
+    Station destinationStation = stations.get(destinationStationName);
+    Set<TrainLine> linesInCommon = new HashSet<>();
 
-    UI.println("Searching for route from "
-        + currentStationName +
-        " to " + destinationStationName +
-        " from " + currentTime + "...");
+    // Print search information
+    UI.printf("Searching for route from %s to %s from %s...\n",
+        currentStationName, destinationStationName, currentTime);
 
-    // Find out if they share a line
+    // Check if the stations are on the same line
+    Set<TrainLine> linesForCurrentStation = currentStation.getTrainLines();
+    Set<TrainLine> linesForDestinationStation = destinationStation.getTrainLines();
+    for (TrainLine trainLine : linesForCurrentStation) {
+      if (linesForDestinationStation.contains(trainLine)) {
+        onSameLine = true;
+        linesInCommon.add(trainLine);
+      }
+    }
+
+    // If the stations are on the same line, find the route
+    if (onSameLine) {
+      findDirectRoute(linesInCommon, currentStation, destinationStation);
+    } else {
+      UI.println("No match. You will have to transfer lines");
+      // TODO: Implement logic for transferring lines
+    }
+  }
+
+  /**
+   * Finds a direct route between two stations on the same line.
+   */
+  private void findDirectRoute(Set<TrainLine> linesInCommon,
+      Station currentStation,
+      Station destinationStation) {
+    for (TrainLine trainLine : linesInCommon) {
+      int currentIndex = trainLine.getStations().indexOf(currentStation);
+      int destinationIndex = trainLine.getStations().indexOf(destinationStation);
+
+      if (currentIndex < destinationIndex) {
+        findServiceForStations(trainLine, currentIndex, destinationIndex);
+      } else {
+        // TODO: Implement logic for changing lines if necessary
+      }
+    }
+  }
+
+  /**
+   * Finds the train service for two stations on the same line.
+   */
+  private void findServiceForStations(TrainLine trainLine,
+      int currentIndex,
+      int destinationIndex) {
+    for (TrainService service : trainLine.getTrainServices()) {
+      int currentTimeInt = Integer.parseInt(currentTime);
+      int currentServiceTime = service.getTimes().get(currentIndex);
+      int destinationServiceTime = service.getTimes().get(destinationIndex);
+
+      // if there are no more services for the day, tell the user
+      if (currentServiceTime == -1 || destinationServiceTime == -1) {
+        UI.println("No more services for the day");
+        // break;
+      }
+
+      if (destinationServiceTime > -1 && currentServiceTime >= currentTimeInt) {
+        printRouteInfo(currentServiceTime, destinationServiceTime);
+        break;
+      }
+    }
+  }
+
+  /**
+   * Prints the route information including time and fare.
+   */
+  private void printRouteInfo(int currentServiceTime, int destinationServiceTime) {
+    UI.printf("Leaves %s at %s\n", currentStationName, addColonToTime(currentServiceTime));
+    UI.printf("Arrives %s at %d\n", destinationStationName, addColonToTime(destinationServiceTime));
 
     Station currentStation = stations.get(currentStationName);
     Station destinationStation = stations.get(destinationStationName);
+    int zoneDifference = Math.max(1, destinationStation.getZone() - currentStation.getZone());
+    double fare = fares.get(zoneDifference);
 
-    Set<TrainLine> linesForStation1 = stations.get(currentStationName).getTrainLines();
-    Set<TrainLine> linesForStation2 = stations.get(destinationStationName).getTrainLines();
-
-    Set<TrainLine> linesInCommon = new HashSet<>();
-
-    for (TrainLine trainLineThroughStation1 : linesForStation1) {
-      if (linesForStation2.contains(trainLineThroughStation1)) {
-        onSameLine = true;
-        linesInCommon.add(trainLineThroughStation1);
-      }
-    }
-
-    if (onSameLine) {
-
-      for (TrainLine trainLine : linesInCommon) {
-        // UI.println(trainLine.toString());
-
-        int currentStationIdx = trainLine.getStations().indexOf(currentStation);
-        int destinationStationIdx = trainLine.getStations().indexOf(destinationStation);
-
-        // TODO: handle behaviour here
-        // if (destinationStationIdx == -1) {
-        // UI.println("This train does not stop at " + destinationStationName);
-        // // break;
-        // // skip this line and try the next line
-        // }
-
-        if (currentStationIdx < destinationStationIdx) {
-          // UI.println("You are travelling in the right direction");
-
-          // find a service at or after the currentTime
-
-          for (TrainService trainService : trainLine.getTrainServices()) {
-
-            if (trainService.getTimes().get(destinationStationIdx) > -1) {
-              // UI.println("Found a service that stops at the destination station");
-
-              if (trainService.getTimes().get(currentStationIdx) >= Integer.parseInt(currentTime)) {
-                // UI.println("Found a service at or after the current time");
-
-                UI.println("Leaves " + currentStationName + " at " + trainService.getTimes().get(currentStationIdx));
-                UI.println("Arrives " + destinationStationName + " at "
-                    + String.valueOf(trainService.getTimes().get(destinationStationIdx)));
-
-                int zoneDifference = destinationStation.getZone() - currentStation.getZone();
-                if (zoneDifference <= 0) {
-                  zoneDifference = 1;
-                }
-                double fare = fares.get(zoneDifference);
-
-                UI.printf("Fare: $%.2f (%d zones)\n", fare, zoneDifference);
-
-                break;
-              }
-
-            } else {
-              // UI.println("This train does not stop at " + destinationStationName);
-            }
-
-          }
-
-        } else {
-          // UI.println("You are travelling in the wrong direction");
-        }
-      }
-    }
-
-    // TODO: handle if there is a -1 on the stattion!
-    // then the train is not stopping at that station
-
-    if (!onSameLine) {
-      UI.println("No match. You will have to transfer lines");
-
-      // TODO: " current_station.line + " to " + destination_station.line
-    }
-
-  }
-
-  public void displayStationsInStatusBar() {
-    UI.printMessage("Current: " + currentStationName + " | " + " Destination: " + destinationStationName);
+    UI.printf("Fare: $%.2f (%d zones)\n", fare, zoneDifference);
   }
 
   public void listAllLinesThroughStation() {
@@ -218,8 +230,9 @@ public class WellyTrains {
     UI.drawImage("data/system-map.png", 0, 0);
   }
 
-  // TODO: fix data for woodsite and matarawa
-
+  /*
+   * Detects if the user has clicked on a train line header or station
+   */
   public void mouseListener(final String action, final double x, final double y) {
 
     if ("pressed".equals(action)) {
@@ -229,11 +242,15 @@ public class WellyTrains {
 
     if ("released".equals(action)) {
 
+      // Code for getting the coordinates of the mouse click for the map
       // UI.println(pressedX + " " + pressedY + " " + x + " " + y);
 
       // UI.printMessage("(x > " + pressedX + ") && (x < " + x + ") && (y > " +
       // pressedY + ") && (y < " + y + ")");
 
+      /*
+       * Detect if the user has clicked on a train line
+       */
       if ((x > 16.0) && (x < 159.0) && (y > 417.0) && (y < 434.0)) {
         // UI.print(trainLines.get("Johnsonville_Wellington"));
         this.printStationsOnTrainLine(trainLines.get("Johnsonville_Wellington"));
@@ -278,19 +295,38 @@ public class WellyTrains {
 
         this.resetGraphicsPane();
         this.currentStationName = v.getName();
-        currentStationBtn.setText("Current Station: " + currentStationName);
+        updateButtons();
+        // currentStationBtn.setText("Current Station: " + currentStationName);
+        // listAllLinesBtn.setText("List All Lines through " + currentStationName);
         drawBoxAroundStationName(v.getName());
         UI.drawRect(v.getLeftEdge(), v.getTopEdge(), v.calculateWidth(), v.calculateHeight());
       }
     });
   }
 
+  /*
+   * Updates the text on the buttons to reflect the current station and
+   * destination
+   */
+  public void updateButtons() {
+    currentStationBtn.setText("Current Station: " + currentStationName);
+    destStationBtn.setText("Destination Stn: " + this.destinationStationName);
+    listAllLinesBtn.setText("List All Lines through " + currentStationName);
+    findRouteBtn.setText("Find a route from " + currentStationName + " to " + destinationStationName);
+  }
+
+  /*
+   * Draws a box around the station name on the map
+   */
   public void drawBoxAroundStationName(String stationName) {
     this.resetGraphicsPane();
     Station s = stations.get(stationName);
     UI.drawRect(s.getLeftEdge(), s.getTopEdge(), s.calculateWidth(), s.calculateHeight());
   }
 
+  /*
+   * Prints all the stations on a train line
+   */
   public void printStationsOnTrainLine(final TrainLine trainLine) {
     UI.println("Stations on the " + trainLine.getName() + " line");
     trainLine.getStations().forEach(station -> {
@@ -299,10 +335,16 @@ public class WellyTrains {
     UI.println();
   }
 
+  /*
+   * Prints the instructions for the user
+   */
   public void printInstructions() {
     UI.println("Click on the train line header to list stations on that line");
   }
 
+  /*
+   * Prints the lines that go through the current station
+   */
   public void printSimpleLinesForStation() {
     UI.println("-- Showing lines through " + currentStationName + " station --");
     final Station s = stations.get(currentStationName);
@@ -311,24 +353,28 @@ public class WellyTrains {
     }
   }
 
+  /*
+   * Prints the lines that go through the current station
+   * and the stations on those lines
+   */
   public void printLinesForStation(final String stationName) {
     final Station s = stations.get(stationName);
     UI.println("Printing Lines that go through " + stationName + " station");
-    UI.println("In Zone " + s.getZone());
+    // UI.println("In Zone " + s.getZone());
     UI.println("On Lines");
     for (final TrainLine tl : s.getTrainLines()) {
       UI.println("- " + tl.getName());
-      for (int i = 0; i < tl.getStations().size(); i++) {
-        UI.print("   " + i + ". ");
+      // for (int i = 0; i < tl.getStations().size(); i++) {
+      // UI.print(" " + i + ". ");
 
-        UI.print(tl.getStations().get(i).getName());
+      // UI.print(tl.getStations().get(i).getName());
 
-        if (tl.getStations().get(i).getName().equals(s.getName())) {
-          UI.println(" ***");
-        } else {
-          UI.println();
-        }
-      }
+      // if (tl.getStations().get(i).getName().equals(s.getName())) {
+      // UI.println(" ***");
+      // } else {
+      // UI.println();
+      // }
+      // }
     }
   }
 
@@ -454,11 +500,8 @@ public class WellyTrains {
           tService = new TrainService(v);
           final String line = sc.nextLine();
           final String[] times = line.split(" ");
-          // UI.println("line " + line);
           boolean isFirstStop = false;
           for (int i = 0; i < times.length; i++) {
-
-            // public void addTime(int time, boolean firstStop){
 
             if (i == 0) {
               isFirstStop = true;
@@ -466,32 +509,36 @@ public class WellyTrains {
               isFirstStop = false;
             }
             tService.addTime(Integer.parseInt(times[i]), isFirstStop);
-            // tService.addTime(Integer.parseInt(times[i]), !(i == 0));
           }
           tl.addTrainService(tService);
-
         }
-
       } catch (final FileNotFoundException e) {
         e.printStackTrace();
       }
-
     });
   }
 
+  /*
+   * List all train lines in the trainLines hashmap
+   * in a nice format
+   */
   public void showAllTrainLines() {
     UI.println("All Train Lines");
     UI.println("---");
     trainLines.forEach((k, v) -> {
-      UI.println(v.getName());
+      UI.println(v.getName().replace("_", " to ").replace("-", " ") + " line");
     });
   }
 
+  /*
+   * List all stations in the stations hashmap without the dash
+   *
+   */
   public void showAllStations() {
     UI.println("All Stations");
     UI.println("---");
     stations.forEach((k, v) -> {
-      UI.println(v.getName() + " in zone " + v.getZone());
+      UI.println(v.getName().replace("-", " "));
     });
   }
 
@@ -519,6 +566,9 @@ public class WellyTrains {
 
   }
 
+  /*
+   * Open the train-lines.data file and build that data into the trainLines
+   */
   public void loadTrainLineData() {
     final String dataFile = "data/train-lines.data";
     Scanner sc;
@@ -542,6 +592,10 @@ public class WellyTrains {
     this.loadStationsOnLine();
   }
 
+  /*
+   * Open the *-stations.data file for each line and build that data into the
+   * train
+   */
   public void loadStationsOnLine() {
     trainLines.forEach((trainLineKey, trainLineVal) -> {
       Scanner sc;
@@ -549,19 +603,12 @@ public class WellyTrains {
         sc = new Scanner(new File("data/" + trainLineKey + "-stations.data"));
         while (sc.hasNext()) {
 
-          // 1. add station to the trainLine
           final TrainLine trainLine = trainLines.get(trainLineKey);
           final String stationName = sc.next();
           final Station s = stations.get(stationName);
           trainLine.addStation(s);
 
           s.addTrainLine(trainLine);
-
-          // Station station = stations.get(trainLineVal);
-          // station.addTrainLine(trainLineVal);
-
-          // UI.print("**" + trainLineKey + "** ");
-          // UI.println(sc.next());
         }
       } catch (final FileNotFoundException e) {
         e.printStackTrace();
@@ -578,10 +625,9 @@ public class WellyTrains {
     if (name == null) {
       return;
     }
-    currentStationName = name;
-    currentStationBtn.setText("Current Station: " + currentStationName);
-    listAllLinesBtn.setText("List All Lines through " + this.currentStationName);
-    drawBoxAroundStationName(currentStationName);
+    this.currentStationName = name;
+    updateButtons();
+    drawBoxAroundStationName(this.currentStationName);
   }
 
   /**
@@ -594,22 +640,17 @@ public class WellyTrains {
     if (name == null) {
       return;
     }
-    UI.println("Setting destination station to " + name);
+    // UI.println("Setting destination station to " + name);
     destinationStationName = name;
-    destStationBtn.setText("Destination Station: " + destinationStationName);
-    // displayCurrentValues();
-    this.displayStationsInStatusBar();
+    updateButtons();
   }
 
   //
   /**
-   * Not my code.
-   *
+   * Not my code
    * Method to get a string from a dialog box with a list of options
    * Written by VUW faculty for COMP103 assignment 2023
-   *
    * Just used for choosing station without typing.
-   *
    * Can also select current station from UI map (Andy's code)
    */
   public String getOptionFromList(final String question, final Collection<String> options) {
@@ -620,9 +661,9 @@ public class WellyTrains {
   }
 
   public static void main(final String[] args) {
-    WellyTrains welllyTrains = new WellyTrains();
-    welllyTrains.setupUserInterface();
-    welllyTrains.loadAllData();
+    WellyTrains wellyTrains = new WellyTrains();
+    wellyTrains.setupUserInterface();
+    wellyTrains.loadAllData();
   }
 
 }
